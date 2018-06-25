b1c336147b690bac2931cbd056c2e4be6fcd4234
Edward Manda <lilkups@yahoo.com>
2018-06-25 17:42:51 -0500

    created a book mark app

--- notesapp/models.py
+++ notesapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Note(models.Model):
+    # TODO: Add user author
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    # TODO: Tagging, Categories
+
+
+class Bookmark(models.Model):
+    # TODO: Add user author
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    url = models.URLField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    # TODO: Tagging, Categories, Description
